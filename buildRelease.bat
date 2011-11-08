cf4667bd4be1148f42bfc207e5b1d626f7f1b871
Tapsa <mikonposti@live.fi>
2011-11-08 17:50:02 +0200

    Updated build bats.

--- buildRelease.bat
+++ buildRelease.bat
@@ -1,5 +1,10 @@
 cd build/
 cmake -DCMAKE_EXE_LINKER_FLAGS="-static-libstdc++ -static-libgcc --disable-shared --enable-static" -DCMAKE_SHARED_LINKER_FLAGS="-static-libstdc++ -static-libgcc --disable-shared --enable-static" -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE:STRING=Release -DwxWidgets_ROOT_DIR:PATH="C:\Cpp\wxWidgets" -DBOOST_LIBRARYDIR:PATH="C:\Cpp\boost_libs" -DBOOST_ROOT:PATH="C:\Cpp\boost_1_47_0" -DZLIB_LIBRARY:FILEPATH="C:\Cpp\zlib\libz.a" -DZLIB_INCLUDE_DIR:PATH="C:\Cpp\zlib" ../
-mingw32-make 
+mingw32-make
 cd ..
+cd ..
+copy "sources\build\AdvancedGenieEditor2.exe" "AGE2\AdvancedGenieEditor2.exe"
+cd AGE2
+strip *
+upx --best *
 PAUSE
