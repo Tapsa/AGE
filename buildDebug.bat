988caad945ca9f6104a5307f160da2861f969a27
Mikko <1067314+Tapsa@users.noreply.github.com>
2011-11-08 17:50:02 +0200

    Updated build bats.

--- buildDebug.bat
+++ buildDebug.bat
@@ -1,10 +1,4 @@
 cd build/
-cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE:STRING=Debug -DwxWidgets_CONFIGURATION:STRING="mswd" -DwxWidgets_ROOT_DIR:PATH="C:\Cpp\wxWidgets" -DBOOST_LIBRARYDIR:PATH="C:\Cpp\boost_libs" -DBOOST_ROOT:PATH="C:\Cpp\boost_1_47_0" -DZLIB_LIBRARY:FILEPATH="C:\Cpp\zlib\zlib1.dll" -DZLIB_INCLUDE_DIR:PATH="C:\Cpp\zlib\include" ../
-mingw32-make 
-cd ..
-cd ..
-copy "sources\build\AdvancedGenieEditor2.exe" "AGE2\AdvancedGenieEditor2.exe"
-cd AGE2
-strip *
-upx --best *
+cmake -DCMAKE_EXE_LINKER_FLAGS="-static-libstdc++ -static-libgcc --disable-shared --enable-static" -DCMAKE_SHARED_LINKER_FLAGS="-static-libstdc++ -static-libgcc --disable-shared --enable-static" -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE:STRING=Debug -DwxWidgets_ROOT_DIR:PATH="C:\Cpp\wxWidgets" -DBOOST_LIBRARYDIR:PATH="C:\Cpp\boost_libs" -DBOOST_ROOT:PATH="C:\Cpp\boost_1_47_0" -DZLIB_LIBRARY:FILEPATH="C:\Cpp\zlib\libz.a" -DZLIB_INCLUDE_DIR:PATH="C:\Cpp\zlib" ../
+mingw32-make
 PAUSE
