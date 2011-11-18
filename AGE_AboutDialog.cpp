9be9cd82365780dd68de457345cc4c9f80756b17
Tapsa <mikonposti@live.fi>
2011-11-18 22:40:49 +0200

    First proper release.

--- AGE_AboutDialog.cpp
+++ AGE_AboutDialog.cpp
@@ -5,7 +5,7 @@
 AGE_AboutDialog::AGE_AboutDialog(wxWindow * parent)
 : wxDialog(parent, -1, "About Advanced Genie Editor", wxDefaultPosition, wxDefaultSize, wxDEFAULT_DIALOG_STYLE | wxNO_DEFAULT)
 {
-	Title = new wxStaticText(this, wxID_ANY, "Advanced Genie Editor\nVersion 2.1 Beta 11\nCopyleft 2011\n\nDevelopers:\nKeisari Tapsa (from 2.0b)\nApre - libgeniedat (from 2.1a)\nEstien Nifo (aka StSB77) (1.0a to 2.0a)");
+	Title = new wxStaticText(this, wxID_ANY, "Advanced Genie Editor\nVersion 2.1\nCopyleft 2011\n\nDevelopers:\nKeisari Tapsa (from 2.0b)\nApre - libgeniedat (from 2.1a)\nEstien Nifo (aka StSB77) (1.0a to 2.0a)");
 	Image = new wxStaticBitmap(this, wxID_ANY, wxBitmap(AboutIcon_xpm));
 	Credits = new wxStaticText(this, wxID_ANY, "Credits:\nYkkrosh - GeniEd 1 source code\nScenario_t_c - GeniEd 2 source code\nAncient Warrior (aka Taichi San) - data file research\nDiGiT - genie file structure (GenieWiki)\nqaz123tfg - help documentation\nLeif Ericson - new icon");
 	AoKHThread = new wxHyperlinkCtrl(this, wxID_ANY, "Age of Kings Heaven AGE forum topic", "http://aok.heavengames.com/cgi-bin/aokcgi/display.cgi?action=st&fn=4&tn=38606&st=recent&f=4,38606,0,365", wxDefaultPosition, wxDefaultSize, wxNO_BORDER | wxHL_CONTEXTMENU | wxHL_ALIGN_LEFT);
