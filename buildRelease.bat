133e37dca1deac73fd0a7912528143674434da66
Tapsa <1067314+Tapsa@users.noreply.github.com>
2012-07-22 13:31:49 +0300

    Udated compiling stuff.

--- buildRelease.bat
+++ buildRelease.bat
@@ -1,5 +1,5 @@
 cd build/
-cmake -DCMAKE_EXE_LINKER_FLAGS="-static-libstdc++ -static-libgcc --disable-shared --enable-static" -DCMAKE_SHARED_LINKER_FLAGS="-static-libstdc++ -static-libgcc --disable-shared --enable-static" -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE:STRING=Release -DwxWidgets_ROOT_DIR:PATH="C:\Cpp\wxWidgets" -DBOOST_LIBRARYDIR:PATH="C:\Cpp\boost_libs" -DBOOST_ROOT:PATH="C:\Cpp\boost_1_47_0" -DZLIB_LIBRARY:FILEPATH="C:\Cpp\zlib\libz.a" -DZLIB_INCLUDE_DIR:PATH="C:\Cpp\zlib" ../
+cmake -DCMAKE_EXE_LINKER_FLAGS="-static-libstdc++ -static-libgcc -static" -DCMAKE_SHARED_LINKER_FLAGS="-static-libstdc++ -static-libgcc -static" -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE:STRING=Release -DwxWidgets_ROOT_DIR:PATH="C:\Cpp\wxWidgets" -DBOOST_LIBRARYDIR:PATH="C:\Cpp\Boost\lib" -DBOOST_INCLUDEDIR:PATH="C:\Cpp\Boost\include" -DZLIB_LIBRARY:FILEPATH="C:\Cpp\zlib\libz.a" -DZLIB_INCLUDE_DIR:PATH="C:\Cpp\zlib" ../
 mingw32-make
 cd ..
 cd ..
