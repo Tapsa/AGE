185785f94e602a6c510df93b8c4af13e64b9e5c4
Tapsa <1067314+Tapsa@users.noreply.github.com>
2012-10-05 17:48:05 +0300

    Civ buttons removed from forbidden list.
    To do:
    Add warning of 30+ civs (auto-copy).
    Fix transporting.
    Make all-effects and all-soundfiles lists multi-selectable.

--- AGE_Frame_cpp/Other.cpp
+++ AGE_Frame_cpp/Other.cpp
@@ -1548,10 +1548,6 @@ void AGE_Frame::OnMenuOption(wxCommandEvent& Event)
 			ShowButtons = Event.IsChecked();
 			if(ShowButtons)
 			{
-				Civs_Add->Enable(true); // Must copy existing civ over the new one
-				Civs_Insert->Enable(true);
-				Civs_Delete->Enable(true);
-				Civs_PasteInsert->Enable(true);
 				Terrains_Add->Enable(true);
 				Terrains_Insert->Enable(true);
 				Terrains_Delete->Enable(true);
@@ -1559,10 +1555,6 @@ void AGE_Frame::OnMenuOption(wxCommandEvent& Event)
 			}
 			else
 			{
-				Civs_Add->Enable(false);
-				Civs_Insert->Enable(false);
-				Civs_Delete->Enable(false);
-				Civs_PasteInsert->Enable(false);
 				Terrains_Add->Enable(false);
 				Terrains_Insert->Enable(false);
 				Terrains_Delete->Enable(false);
