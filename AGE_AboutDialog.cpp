b4c2ca1fb4fa8c833d1f479c32f2b2e230de8468
Tapsa <mikonposti@live.fi>
2012-11-09 23:46:20 +0200

    Revert "Start of large refactoring."
    This reverts commit 7b99cc2ac05e52972c531b1b26d8e2797a88e558.

--- AGE_AboutDialog.cpp
+++ AGE_AboutDialog.cpp
@@ -1,5 +1,6 @@
+/* AGEAbouDialog.cpp */
+
 #include "AGE_AboutDialog.h"
-#include "AboutIcon.xpm"
 
 AGE_AboutDialog::AGE_AboutDialog(wxWindow * parent)
 : wxDialog(parent, -1, "About Advanced Genie Editor", wxDefaultPosition, wxDefaultSize, wxDEFAULT_DIALOG_STYLE | wxNO_DEFAULT)
