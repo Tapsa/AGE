5323d396fc9bf21f4cb40fb86250706e3a16da7b
Mikko <mikko.partonen@metropolia.fi>
2014-12-02 21:42:43 +0200

    Added common gui for 84 shared bytes in tech trees.

--- AGE_ComboBoxes.h
+++ AGE_ComboBoxes.h
@@ -3,57 +3,6 @@
 #ifndef AGE_ComboBoxes_h
 #define AGE_ComboBoxes_h
 
-/*class AGEListCtrl: public wxListView
-{
-public:
-	AGEListCtrl(wxWindow *parent, const wxSize &size):
-	wxListView(parent, wxID_ANY, wxDefaultPosition, size, wxLC_VIRTUAL | wxLC_REPORT | wxLC_NO_HEADER)
-	{
-		AppendColumn("");
-	}
-
-	wxArrayString labels;
-
-	void Listing(wxArrayString &items, list<void*> &data)
-	{
-		SetItemCount(items.GetCount());
-		labels = items;
-	}
-
-	long GetSelections(wxArrayInt &selections) const
-	{
-		selections.Clear();
-		long first = GetFirstSelected();
-		if(first != -1)
-		{
-			selections.Add(first);
-			long next = GetNextSelected(first);
-			while(next != -1)
-			{
-				selections.Add(next);
-				next = GetNextSelected(next);
-			}
-		}
-		selections.GetCount();
-	}
-
-	void SetSelection(long n)
-	{
-		Select(n, true);
-	}
-
-	void SetFirstItem(long n)
-	{
-		EnsureVisible(n);
-	}
-
-private:
-	virtual wxString OnGetItemText(long item, long column) const
-	{
-		return labels[item];
-	}
-};*/
-
 class AGEComboBox: public wxOwnerDrawnComboBox, public AGELinkedBox
 {
 public:
