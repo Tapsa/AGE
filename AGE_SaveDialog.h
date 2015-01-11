7c8e1309475cd2c2075494eaab776d860392a966
Tapsa <mikonposti@live.fi>
2015-01-11 14:09:39 +0200

    Unsaved prompt, cmd line dat location.

--- AGE_SaveDialog.h
+++ AGE_SaveDialog.h
@@ -1,9 +1,8 @@
-#include "Common.h"
+#include "AGE_OpenSave.h"
 
-class AGE_SaveDialog: public wxDialog
+class AGE_SaveDialog: public AGE_OpenSave
 {
-	public:
-
+public:
 	AGE_SaveDialog(wxWindow *parent);
 
 	/* Events */
@@ -20,42 +19,11 @@ class AGE_SaveDialog: public wxDialog
 	void OnSelectLang(wxCommandEvent &Event);
 	void OnSelectLangX1(wxCommandEvent &Event);
 	void OnSelectLangX1P1(wxCommandEvent &Event);
-//	void OnSelectDrs(wxCommandEvent &Event);
 	void OnOK(wxCommandEvent &Event);
 
 	/* Member Variables */
 
-	wxBoxSizer *SaveArea;
-	wxBoxSizer *MainSave;
-	wxBoxSizer *SaveDefaults;
-	wxFlexGridSizer *SaveLayout;
-	wxBoxSizer *SaveButtons;
-	wxButton *Button_DefaultAoE;
-	wxButton *Button_DefaultRoR;
-	wxButton *Button_DefaultAoK;
-	wxButton *Button_DefaultTC;
-	wxButton *Button_DefaultAoKHD;
-	wxButton *Button_DefaultSWGB;
-	wxButton *Button_DefaultCC;
-	wxStaticText *Text_GenieVer;
-	wxComboBox *CheckBox_GenieVer;
-	wxTextCtrl *DriveLetterBox;
-	wxTextCtrl *LanguageBox;
-	wxCheckBox *CheckBox_CustomDefault;
-	wxDirPickerCtrl *Path_CustomDefault;
 	wxCheckBox *CheckBox_DatFileLocation;
-	wxFilePickerCtrl *Path_DatFileLocation;
 	wxCheckBox *CheckBox_UnzFileLocation;
-	wxFilePickerCtrl *Path_UnzFileLocation;
 	wxCheckBox *CheckBox_ApfFileLocation;
-	wxFilePickerCtrl *Path_ApfFileLocation;
-	wxCheckBox *CheckBox_LangFileLocation;
-	wxFilePickerCtrl *Path_LangFileLocation;
-	wxCheckBox *CheckBox_LangX1FileLocation;
-	wxFilePickerCtrl *Path_LangX1FileLocation;
-	wxCheckBox *CheckBox_LangX1P1FileLocation;
-	wxFilePickerCtrl *Path_LangX1P1FileLocation;
-	wxCheckBox *CheckBox_LangWrite;
-	wxButton *SaveButtonOK;
-	wxButton *SaveButtonCancel;
 };
