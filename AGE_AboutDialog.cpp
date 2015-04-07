39796cdd58e67695840900f3c1bc2b13a963498c
Mikko <mikko.partonen@metropolia.fi>
2015-04-07 23:35:42 +0300

    Terrain amount hack for 47 terrains.
    Sorted unit search filters too.

--- AGE_AboutDialog.cpp
+++ AGE_AboutDialog.cpp
@@ -1,7 +1,7 @@
 #include "AGE_AboutDialog.h"
 #include "AboutIcon.xpm"
 
-const wxString AGE_AboutDialog::AGE_VER = "2015.3.9";
+const wxString AGE_AboutDialog::AGE_VER = "2015.4.7";
 
 AGE_AboutDialog::AGE_AboutDialog(wxWindow *parent)
 : wxDialog(parent, -1, "About Advanced Genie Editor", wxDefaultPosition, wxDefaultSize, wxDEFAULT_DIALOG_STYLE | wxNO_DEFAULT)
@@ -10,8 +10,8 @@ AGE_AboutDialog::AGE_AboutDialog(wxWindow *parent)
 	Image = new wxStaticBitmap(this, wxID_ANY, wxBitmap(AboutIcon_xpm));
 	Credits = new wxStaticText(this, wxID_ANY, "Credits:\nYkkrosh - GeniEd 1 source code\nScenario_t_c - GeniEd 2 source code\nAlexandra \"Taichi San\", DarkRain654 - data file research\nDiGiT, JustTesting1234 - genie file structure\nSarthos - important help\nBF_Tanks - some help\nDonnieboy, Sarn, chab - tooltip texts\nLeif Ericson - new icon");
 	AoKHThread = new wxHyperlinkCtrl(this, wxID_ANY, "Age of Kings Heaven AGE forum topic", "http://aok.heavengames.com/cgi-bin/aokcgi/display.cgi?action=st&fn=4&tn=41300&st=recent&f=4,41300,0,365", wxDefaultPosition, wxDefaultSize, wxNO_BORDER | wxHL_CONTEXTMENU | wxHL_ALIGN_LEFT);
-	UPXInfo = new wxStaticText(this, wxID_ANY, "Compressed with:");
-	UPXLink = new wxHyperlinkCtrl(this, wxID_ANY, "The Ultimate Packer for eXecutables", "http://upx.sourceforge.net", wxDefaultPosition, wxDefaultSize, wxNO_BORDER | wxHL_CONTEXTMENU | wxHL_ALIGN_LEFT);
+	//UPXInfo = new wxStaticText(this, wxID_ANY, "Compressed with:");
+	//UPXLink = new wxHyperlinkCtrl(this, wxID_ANY, "The Ultimate Packer for eXecutables", "http://upx.sourceforge.net", wxDefaultPosition, wxDefaultSize, wxNO_BORDER | wxHL_CONTEXTMENU | wxHL_ALIGN_LEFT);
 
 	MainLeft = new wxBoxSizer(wxVERTICAL);
 	MainRight = new wxBoxSizer(wxVERTICAL);
@@ -27,9 +27,8 @@ AGE_AboutDialog::AGE_AboutDialog(wxWindow *parent)
 	MainRight->Add(Credits, 1, wxEXPAND);
 	MainRight->Add(-1, 15);
 	MainRight->Add(AoKHThread, 0, wxEXPAND);
-	MainRight->Add(-1, 15);
-	MainRight->Add(UPXInfo, 0, wxEXPAND);
-	MainRight->Add(UPXLink, 0, wxEXPAND);
+	//MainRight->Add(UPXInfo, 0, wxEXPAND);
+	//MainRight->Add(UPXLink, 0, wxEXPAND);
 	MainRight->Add(-1, 15);
 
 	MainAbout->Add(15, -1);
