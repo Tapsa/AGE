0558c77b9d442a5e035daea3c3521ef1bf4644ef
Mikko <1067314+Tapsa@users.noreply.github.com>
2015-07-27 18:42:26 +0300

    Decompress can try any file type.
    Both my decompress tool and aokts decompress tool fails to decompress
    scenarios.
    I think that the uncompressed header is the problem.

--- AGE_AboutDialog.cpp
+++ AGE_AboutDialog.cpp
@@ -1,14 +1,14 @@
 #include "AGE_AboutDialog.h"
 #include "AboutIcon.xpm"
 
-const wxString AGE_AboutDialog::AGE_VER = "2015.7.21";
+const wxString AGE_AboutDialog::AGE_VER = "2015.7.27";
 
 AGE_AboutDialog::AGE_AboutDialog(wxWindow *parent)
 : wxDialog(parent, -1, "About Advanced Genie Editor", wxDefaultPosition, wxDefaultSize, wxDEFAULT_DIALOG_STYLE | wxNO_DEFAULT)
 {
     Title = new wxStaticText(this, wxID_ANY, "Advanced Genie Editor\nVersion "+AGE_VER+"\nCopyleft 2011 - 2015\n\nDevelopers:\nMikko \"Tapsa\" P, since 2.0b\nApre - genieutils, 2.1a to 3.1\nEstien Nifo aka StSB77, 1.0a to 2.0a");
     Image = new wxStaticBitmap(this, wxID_ANY, wxBitmap(AboutIcon_xpm));
-    Credits = new wxStaticText(this, wxID_ANY, "Credits:\nYkkrosh - GeniEd 1 source code\nScenario_t_c - GeniEd 2 source code\nAlexandra \"Taichi San\", DarkRain654 - data file research\nDiGiT, JustTesting1234 - genie file structure\nCysion, Skybox Labs, Sarthos - important help\nBF_Tanks - some help\nDonnieboy, Sarn, chab - tooltip texts\nLeif Ericson - new icon");
+    Credits = new wxStaticText(this, wxID_ANY, "Credits:\nYkkrosh - GeniEd 1 source code\nScenario_t_c - GeniEd 2 source code\nAlexandra \"Taichi San\", DarkRain654 - data file research\nDiGiT, JustTesting1234, AOHH - genie file structure\nCysion, Skybox Labs, Sarthos - important help\nBF_Tanks - some help\nDonnieboy, Sarn, chab - tooltip texts\nLeif Ericson - new icon");
     AoKHThread = new wxHyperlinkCtrl(this, wxID_ANY, "Age of Kings Heaven AGE forum topic", "http://aok.heavengames.com/cgi-bin/forums/display.cgi?action=st&fn=9&tn=44059&st=recent&f=9,44059,0,365", wxDefaultPosition, wxDefaultSize, wxNO_BORDER | wxHL_CONTEXTMENU | wxHL_ALIGN_LEFT);
     //UPXInfo = new wxStaticText(this, wxID_ANY, "Compressed with:");
     //UPXLink = new wxHyperlinkCtrl(this, wxID_ANY, "The Ultimate Packer for eXecutables", "http://upx.sourceforge.net", wxDefaultPosition, wxDefaultSize, wxNO_BORDER | wxHL_CONTEXTMENU | wxHL_ALIGN_LEFT);
--- buildRelease.bat
+++ buildRelease.bat
@@ -1,5 +1,5 @@
 cd buildR/
-cmake -j4 -DCMAKE_EXE_LINKER_FLAGS="-static-libstdc++ -static-libgcc -static" -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DwxWidgets_ROOT_DIR:PATH="C:\Cpp\wxWidgets" -DEXTERN_DIR:PATH="C:\Users\Tapsa" ../
+cmake -j4 -DCMAKE_EXE_LINKER_FLAGS="-static-libstdc++ -static-libgcc -static" -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DwxWidgets_ROOT_DIR:PATH="C:\Cpp\wxWidgets" -DEXTERN_DIR:PATH="%UserProfile%" ../
 mingw32-make
 copy "AdvancedGenieEditor3.exe" "C:\Juttu\AGE\AGE2\AdvancedGenieEditor3.exe"
 cd "C:\Juttu\AGE\AGE2"
