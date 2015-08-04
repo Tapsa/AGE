f153342d1c69556275a21106f95a7b5146896a47
Tapsa <mikonposti@live.fi>
2015-08-04 13:19:27 +0300

    DRS files can be (un)loaded from a button.

--- AGE_SFML.cpp
+++
@@ -1,9 +0,0 @@
-/* AGE_SFML.cpp */
-
-#include "AGE_SFML.h"
-
-BEGIN_EVENT_TABLE(wxSFMLCanvas, wxControl)
-	EVT_IDLE(wxSFMLCanvas::OnIdle)
-	EVT_PAINT(wxSFMLCanvas::OnPaint)
-	EVT_ERASE_BACKGROUND(wxSFMLCanvas::OnEraseBackground)
-END_EVENT_TABLE()
