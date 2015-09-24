26b64fff770300f2f5949dd0cebc7c5f1311fd63
Mikko <mikko.partonen@metropolia.fi>
2015-09-24 12:56:12 +0300

    House graphics with fire animate properly.

--- buildReleaseTest.bat
+++ buildReleaseTest.bat
@@ -1,5 +1,6 @@
 cd buildR/
 cmake -j4 -DCMAKE_EXE_LINKER_FLAGS="-static-libstdc++ -static-libgcc -static" -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DwxWidgets_ROOT_DIR:PATH="C:\Cpp\wxWidgets" -DEXTERN_DIR:PATH="%UserProfile%" ../
 mingw32-make
+strip AdvancedGenieEditor3.exe
 cd ..
 PAUSE
