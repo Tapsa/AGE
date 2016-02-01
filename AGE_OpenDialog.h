7008c313ff38aaa436c42a23ab370c68d5aea7e9
Tapsa <mikonposti@live.fi>
2016-02-01 17:29:40 +0200

    Work on open/save windows.

--- AGE_OpenDialog.h
+++ AGE_OpenDialog.h
@@ -16,11 +16,6 @@ public:
     void OnDefaultAoP(wxCommandEvent &event);
     void OnDefaultSWGB(wxCommandEvent &event);
     void OnDefaultCC(wxCommandEvent &event);
-    void OnChangeDatRadio(wxCommandEvent &event);
-    void OnSelectLang(wxCommandEvent &event);
-    void OnSelectLangX1(wxCommandEvent &event);
-    void OnSelectLangX1P1(wxCommandEvent &event);
-    void OnOK(wxCommandEvent &event);
     void OnDecompress(wxCommandEvent &event);
     void OnRecent(wxCommandEvent &event);
 
@@ -30,10 +25,6 @@ public:
     wxButton *Button_RawDecompress;
     wxFilePickerCtrl *Path_RawDecompress, *Path_DRS3;
     wxTextCtrl *TerrainsBox;
-    wxCheckBox *CheckBox_DRSPath, *CheckBox_DRSPath2, *CheckBox_DRSPath3;
+    AGE_PairedCheckBox *CheckBox_DRSPath, *CheckBox_DRSPath2, *CheckBox_DRSPath3;
     wxDirPickerCtrl *Path_DRS, *Path_DRS2;
-
-#ifdef WIN32
-    void OnPathFromRegistry(wxCommandEvent &event);
-#endif
 };
