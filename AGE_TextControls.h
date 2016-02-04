516fda4b0e84ab67535bb7e51569459fa56ebacf
Tapsa <mikonposti@live.fi>
2016-02-04 18:01:21 +0200

    Binded some hotkeys.
    There seems to be fatal error in genieutils when two different game
    versions are read in two editor windows in the same process. Open TC in
    first window, then open AoE in second window. It is clear from map tab
    that there is some massive error in reading the data around the
    terrains.

--- AGE_TextControls.h
+++ AGE_TextControls.h
@@ -35,7 +35,7 @@ public:
     wxTextCtrl(parent, wxID_ANY, "", wxDefaultPosition, wxSize(width, 20), wxTE_PROCESS_ENTER){edits = 0;}
 
     static AGETextCtrl* init(const ContainerType type, vector<AGETextCtrl*> *group,
-        wxFrame *frame, DelayedPopUp *popUp, wxWindow *parent, short length = 0);
+        wxFrame *frame, DelayedPopUp *editor, wxWindow *parent, short length = 0);
     virtual int SaveEdits(bool forced = false)=0;
     virtual void update()
     {
@@ -51,7 +51,7 @@ public:
             }
             return;
         }
-        curFileLoaded = AGETextCtrl::fileLoaded[window];
+        editedFileId = editor->loadedFileId;
         replenish();
         Enable(true);
     }
@@ -72,12 +72,8 @@ public:
     inline void clear(){container.clear();}
     inline void prepend(void* data){container.push_back(data);}
     void setMaxChars(unsigned short size){maxSize = size;}
-    DelayedPopUp *popUp;
 
     static const wxString BATCHWARNING, BWTITLE, IETITLE;
-    // Make these window specific
-    static vector<bool> hexMode, accurateFloats;
-    static vector<int> unSaved, fileLoaded;
     vector<AGELinkedBox*> LinkedBoxes; // These are for check and combo boxes.
 
 protected:
@@ -108,15 +104,16 @@ protected:
         {
             (*it)->update(casted);
         }
-        frame->SetStatusText("Edits: "+lexical_cast<string>(AGETextCtrl::unSaved[window])+" + "+lexical_cast<string>(edits), 3);
-        AGETextCtrl::unSaved[window] += edits;
+        frame->SetStatusText("Edits: "+lexical_cast<string>(editor->unSaved)+" + "+lexical_cast<string>(edits), 3);
+        editor->unSaved += edits;
         edits = 0;
     }
 
     wxFrame* frame;
+    DelayedPopUp *editor;
     ContainerType type;
     vector<void*> container; // Change to forward_list. No it's slower than vector.
-    int curFileLoaded, window, edits;
+    int editedFileId, edits;
     unsigned short maxSize;
 };
 
@@ -139,11 +136,11 @@ public:
 class TextCtrl_Byte: public AGETextCtrl
 {
 public:
-    TextCtrl_Byte(wxFrame *frame, int window, wxWindow *parent, bool petit = false):
+    TextCtrl_Byte(wxFrame *frame, DelayedPopUp *editor, wxWindow *parent, bool petit = false):
     AGETextCtrl(parent, petit ? 30 : 50)
     {
         this->frame = frame;
-        this->window = window;
+        this->editor = editor;
         SetBackgroundColour(wxColour(255, 235, 215));
         Connect(GetId(), wxEVT_KILL_FOCUS, wxFocusEventHandler(TextCtrl_Byte::OnKillFocus));    // Must-have
         Connect(GetId(), wxEVT_COMMAND_TEXT_ENTER, wxCommandEventHandler(TextCtrl_Byte::OnEnter));
@@ -155,11 +152,11 @@ public:
 class TextCtrl_UByte: public AGETextCtrl
 {
 public:
-    TextCtrl_UByte(wxFrame *frame, int window, wxWindow *parent):
+    TextCtrl_UByte(wxFrame *frame, DelayedPopUp *editor, wxWindow *parent):
     AGETextCtrl(parent, 50)
     {
         this->frame = frame;
-        this->window = window;
+        this->editor = editor;
         SetBackgroundColour(wxColour(255, 235, 215));
         Connect(GetId(), wxEVT_KILL_FOCUS, wxFocusEventHandler(TextCtrl_UByte::OnKillFocus));
         Connect(GetId(), wxEVT_COMMAND_TEXT_ENTER, wxCommandEventHandler(TextCtrl_UByte::OnEnter));
@@ -171,11 +168,11 @@ public:
 class TextCtrl_Float: public AGETextCtrl
 {
 public:
-    TextCtrl_Float(wxFrame *frame, int window, wxWindow *parent, bool petit = false):
+    TextCtrl_Float(wxFrame *frame, DelayedPopUp *editor, wxWindow *parent, bool petit = false):
     AGETextCtrl(parent, petit ? 70 : 100)
     {
         this->frame = frame;
-        this->window = window;
+        this->editor = editor;
         SetBackgroundColour(wxColour(255, 225, 255));
         Connect(GetId(), wxEVT_KILL_FOCUS, wxFocusEventHandler(TextCtrl_Float::OnKillFocus));
         Connect(GetId(), wxEVT_COMMAND_TEXT_ENTER, wxCommandEventHandler(TextCtrl_Float::OnEnter));
@@ -187,11 +184,11 @@ public:
 class TextCtrl_Long: public AGETextCtrl
 {
 public:
-    TextCtrl_Long(wxFrame *frame, int window, wxWindow *parent, bool petit = false):
+    TextCtrl_Long(wxFrame *frame, DelayedPopUp *editor, wxWindow *parent, bool petit = false):
     AGETextCtrl(parent, petit ? 50 : 100)
     {
         this->frame = frame;
-        this->window = window;
+        this->editor = editor;
         SetBackgroundColour(wxColour(215, 255, 255));
         Connect(GetId(), wxEVT_KILL_FOCUS, wxFocusEventHandler(TextCtrl_Long::OnKillFocus));
         Connect(GetId(), wxEVT_COMMAND_TEXT_ENTER, wxCommandEventHandler(TextCtrl_Long::OnEnter));
@@ -203,11 +200,11 @@ public:
 class TextCtrl_Short: public AGETextCtrl
 {
 public:
-    TextCtrl_Short(wxFrame *frame, int window, wxWindow *parent, bool petit = false):
+    TextCtrl_Short(wxFrame *frame, DelayedPopUp *editor, wxWindow *parent, bool petit = false):
     AGETextCtrl(parent, petit ? 50 : 100)
     {
         this->frame = frame;
-        this->window = window;
+        this->editor = editor;
         SetBackgroundColour(wxColour(210, 230, 255));
         Connect(GetId(), wxEVT_KILL_FOCUS, wxFocusEventHandler(TextCtrl_Short::OnKillFocus));
         Connect(GetId(), wxEVT_COMMAND_TEXT_ENTER, wxCommandEventHandler(TextCtrl_Short::OnEnter));
@@ -219,11 +216,11 @@ public:
 class TextCtrl_UShort: public AGETextCtrl
 {
 public:
-    TextCtrl_UShort(wxFrame *frame, int window, wxWindow *parent):
+    TextCtrl_UShort(wxFrame *frame, DelayedPopUp *editor, wxWindow *parent):
     AGETextCtrl(parent, 100)
     {
         this->frame = frame;
-        this->window = window;
+        this->editor = editor;
         SetBackgroundColour(wxColour(210, 230, 255));
         Connect(GetId(), wxEVT_KILL_FOCUS, wxFocusEventHandler(TextCtrl_UShort::OnKillFocus));
         Connect(GetId(), wxEVT_COMMAND_TEXT_ENTER, wxCommandEventHandler(TextCtrl_UShort::OnEnter));
@@ -235,11 +232,11 @@ public:
 class TextCtrl_String: public AGETextCtrl
 {
 public:
-    TextCtrl_String(wxFrame *frame, int window, wxWindow *parent, unsigned short CLength = 0):
+    TextCtrl_String(wxFrame *frame, DelayedPopUp *editor, wxWindow *parent, unsigned short CLength = 0):
     AGETextCtrl(parent, -1)
     {
         this->frame = frame;
-        this->window = window;
+        this->editor = editor;
         maxSize = CLength;
         SetBackgroundColour(wxColour(220, 255, 220));
         Connect(GetId(), wxEVT_KILL_FOCUS, wxFocusEventHandler(TextCtrl_String::OnKillFocus));
