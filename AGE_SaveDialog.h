a34110efbc6762c28b6c4a7c69300a523fe7c84d
Mikko <mikko.partonen@metropolia.fi>
2016-02-18 14:31:34 +0200

    Language file paths editable for AoK HD.

--- AGE_SaveDialog.h
+++ AGE_SaveDialog.h
@@ -6,5 +6,8 @@ class AGE_SaveDialog: public AGE_OpenSave
 public:
     AGE_SaveDialog(wxWindow *parent);
 
+    void OnDefaultAoKHD(wxCommandEvent &event);
+    void OnDefaultAoP(wxCommandEvent &event);
+
     wxCheckBox *SyncWithReadPaths;
 };
