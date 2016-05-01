23d584edb19af71175dc15ed399c605da7566f85
Mikko <mikko.partonen@metropolia.fi>
2016-05-01 18:10:12 +0300

    Better compiling instructions.

--- buildRelease.bat
+++ buildRelease.bat
@@ -7,7 +7,9 @@ SET iconvLL="C:\Cpp\mingw-w64\mingw32\i686-w64-mingw32\lib\libiconv.a"
 cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DwxWidgets_ROOT_DIR:PATH="%wxWidgetsRF%" -DZLIB_INCLUDE_DIR="%zlibIF%" -DZLIB_LIBRARY="%zlibLL%" -DICONV_INCLUDE_DIR="%iconvIF%" -DICONV_LIBRARIES="%iconvLL%" -DEXTERN_DIR:PATH="%UserProfile%" ../
 mingw32-make -j4
 strip AdvancedGenieEditor3.exe
-copy "AdvancedGenieEditor3.exe" "C:\Juttu\AGE\AGE2\AdvancedGenieEditor3.exe"
+strip extern\genieutils\libgenieutils.dll
+copy AdvancedGenieEditor3.exe "C:\Juttu\AGE\AGE2\AdvancedGenieEditor3.exe"
+copy extern\genieutils\libgenieutils.dll "C:\Juttu\AGE\AGE2\libgenieutils.dll"
 cd "C:\Juttu\AGE\AGE2"
 ::upx --best *
 PAUSE
