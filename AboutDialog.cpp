dcb6312bf42e6393727d34cf2e74f926b7ef9c6c
Mikko <1067314+Tapsa@users.noreply.github.com>
2025-06-03 16:48:10 +0300

    Fixed display logic of some editing boxes for AoE 2 DE.

--- AboutDialog.cpp
+++ AboutDialog.cpp
@@ -9,7 +9,7 @@
 #include "Licenses/SFML.h"
 #include "Licenses/wxWidgets.h"
 
-const wxString AboutDialog::AGE_VER = "2025.4.28";
+const wxString AboutDialog::AGE_VER = "2025.6.3";
 
 AboutDialog::AboutDialog(wxWindow *parent, const wxFont &font)
     : wxDialog(parent, -1, "About Advanced Genie Editor", wxDefaultPosition, wxDefaultSize, wxDEFAULT_DIALOG_STYLE | wxNO_DEFAULT)
